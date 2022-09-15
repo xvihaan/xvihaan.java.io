c98de4aaa8cfcf22262ff2e301946bd84cd3f232
xvihaan <112604480+xvihaan@users.noreply.github.com>
2022-09-16 00:11:20 +0900

    Create MyHouse.java

---
+++ MyHouse.java
@@ -0,0 +1,19 @@
+<!doctype html> <!--MyHouse.java -->
+<html>
+<head>
+ <meta charset="utf-8"/>
+ <meta name="viewport" content="width=device-width,initial-scale=1"/>
+ <title>javaro.house</title>
+ <style>
+ a:link {color:blue; text-decoration:none;} a:visited { color:purple; text-decoration:none;}
+ a:hover { text-decoration:underline;} a:active {color:red; text-decoration:none;}
+ </style>
+</head>
+<body>
+ <h2>MyHouse.java</h2>
+  <h3>소스 코드</h3>
+<img src="https://xvihaan.github.io/hw1_1_1_source.jpg">
+<h3>실행 예시</h3>
+<img src="https://xvihaan.github.io/hw1_1_1_result.jpg">
+</body>
+</html>
